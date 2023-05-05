1467fc9630afb373d8f5987e260acbea078dade8
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-05 15:12:05 -0700

    Update Find-the-middle-element.js

--- Find-the-middle-element.js
+++ Find-the-middle-element.js
@@ -17,3 +17,15 @@ function gimme(a) {
 }
 
 // or
+
+const gimme = function (inputArray) {
+  let index = 0;
+  let max = Math.max(...inputArray);
+  let min = Math.min(...inputArray);
+  for(let i = 0; i < inputArray.length; i++){
+    if(inputArray[i] !== max && inputArray[i] !== min){        
+      index = i;
+    }
+  }
+  return index;
+};
